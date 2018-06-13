f81f73d126747d15f2dc661ec9468d3daa7912f2
charleswang234 <wang.charles234@gmail.com>
2018-06-13 16:01:38 +0000

    implemeted a delete operation and routed

--- express_server.js
+++ express_server.js
@@ -15,16 +15,20 @@ function generateRandomString() {
   return generatedString;
 }
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+
+// **********************************************************************
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// **********************************************************************
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase};
@@ -41,6 +45,12 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  let templateVars = { urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = {shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
@@ -48,9 +58,10 @@ app.get("/urls/:id", (req, res) => {
 
 app.post("/urls", (req, res) => {
   var randomString = generateRandomString();
-  urlDatabase[randomString] = req.body.longURL;
+  urlDatabase[randomString] = req.body.longURL; //uses bodyparser
   res.redirect("http://localhost:8080/urls/" + randomString);
-})
+});
+
 
 
 
